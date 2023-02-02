ed7a45e44ce23092fdbcf7b63143bbce69ceb6f4
aum9518 <abduganiuulumanasbek@gmail.com>
2023-02-02 22:47:59 +0600

    HomeWork#11

---
+++ src/Account.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Account {
+    String name;
+    double balance;
+
+public double deposit(double c){
+    balance+=c;
+    return c;
+}
+    public double withdrawal(double sum){
+    balance-=sum;
+        return sum;
+    }
+     double transfer(){
+    double num = 200;
+    return num;
+
+    }
+}
