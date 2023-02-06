ee35c94f44c69d8b8315a63fb004616cd7ac59ba
aum9518 <abduganiuulumanasbek@gmail.com>
2023-02-06 14:18:06 +0600

    HomeWork#12

--- src/Country.java
+++ src/Country.java
@@ -1,5 +1,38 @@
 public class Country {
     private String nameOfCountry;
-    City nameOfCity;
-    Adress adress;
+   private int populationOfCountry;
+   private String nameOfPresident;
+   private String currency;
+   private String language;
+   public void setNameOfCountry(String nameOfCountry){
+       this.nameOfCountry=nameOfCountry;
+   }
+   public void setPopulationOfCountry(int populationOfCountry){
+       this.populationOfCountry=populationOfCountry;
+   }
+   public void setNameOfPresident(String nameOfPresident){
+       this.nameOfPresident= nameOfPresident;
+   }
+   public void setCurrency(String currency){
+       this.currency=currency;
+   }
+   public void setLanguage(String language){
+       this.language=language;
+   }
+   public String getNameOfCountry(){
+       return nameOfCountry;
+   }
+   public int getPopilationOfCountry(){
+       return populationOfCountry;
+   }
+   public String getNameOfPresident(){
+       return nameOfPresident;
+   }
+   public String getCurrency (){
+       return currency;
+   }
+   public String getLanguage(){
+       return language;
+   }
+
 }
